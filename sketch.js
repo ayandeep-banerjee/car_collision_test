cac4b7388f8ec9fee042094fec80f39eed93bd49
Ayandeep Banerjee <73791282+ayandeep-banerjee@users.noreply.github.com>
2020-11-15 17:41:31 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var car,wall;
+var speed,weight;
+var deformation
+
+
+
+function setup() {
+  createCanvas(1600,400); 
+
+  speed=random(55,90);
+  weight=random(400,1500);
+
+  car=createSprite(50,200,50,50);
+  wall=createSprite(1500,200,60,height/2);
+
+  wall.shapeColor=color(80,80,80);
+
+  car.velocityX = speed;
+
+  deformation=0.5*weight*speed*speed/22500
+
+}
+
+function draw() {
+  background(255,220,255);
+  car.collide(wall);
+
+  console.log(deformation);
+
+ 
+
+
+
+  
+ 
+  if (deformation<100) {
+    car.shapeColor=color(0,255,0);
+  }
+
+  if (deformation>100 && deformation<180) {
+    car.shapeColor=color(230,230,0);
+  }
+
+  if (deformation>180) {
+    car.shapeColor=color(255,0,0);
+  }
+
+
+  drawSprites();
+}
